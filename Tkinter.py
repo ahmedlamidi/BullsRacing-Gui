f47f1a2e466b056c254a31649ceead7676d4e633
ahmedlamidi <ahmedlamidi@usf.edu>
2024-02-04 01:28:55 -0500

    graph

--- Tkinter.py
+++ Tkinter.py
@@ -1,7 +1,11 @@
 import tkinter as tk 
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 from Models import Channel
 
-Total_channels = []
+Total_channels = {"none": 0}
 unique = 0
 
 class MainPage(tk.Tk):
@@ -78,9 +82,10 @@ class AddChannel(tk.Frame):
         def add_channel_button():
             global unique
             if selected_data_type.get() and selected_location.get() and selected_name.get():
-                Total_channels.append(Channel.Channel(channel_name=selected_name.get(),
+                new_channel = Channel.Channel(channel_name=selected_name.get(),
                         location = selected_location.get(), data_type=selected_data_type.get(),
-                        hertz_rate = 1, data = [], time = [], unique_id = unique))
+                        hertz_rate = 1, data = [], time = [], unique_id = unique)
+                Total_channels[str(new_channel)] = new_channel
                 unique += 1
                 unique_channel_var.set(unique_channel_var.get() + 1)
             reset_drop_down()
@@ -129,6 +134,9 @@ class AddChannel(tk.Frame):
         location_label = tk.Label(channel_location_frame,text="Select a location for the channel: ")
         location_label.pack(side=tk.LEFT)
 
+
+
+
         button_add_channel = tk.Button(self, text="Add new channel",
                   command=add_channel_button)
 
@@ -158,6 +166,53 @@ class PlotChannel(tk.Frame):
         label = tk.Label(self, text="Plot a channel")
         label.pack(pady=10,padx=10)
 
+        graph = Figure(figsize=(4, 4), dpi=100)
+        canvas = FigureCanvasTkAgg(graph, self)
+        canvas_2 = graph.add_subplot(111)
+        canvas_2.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        canvas_2.plot([1,2,3,4,5,6,7,8],[10,12,2,6,16,18,6,10])
+        canvas_2.plot([1,2,3,4,5,6,7,8],[15,18,3,9,24,27,9,15])
+        canvas_2.plot([1,2,3,4,5,6,7,8],[1000,1000,1000,1000,1000,1000,1000,1000])
+
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
+        frame_select = tk.Frame(self)
+
+
+        first_channel_label = tk.Label(frame_select, text="Select 1st:", fg='#00f')
+        first_channel_label.pack(side=tk.LEFT)
+        first_channel_select = tk.StringVar(value="")
+        first_channel= tk.OptionMenu(frame_select, first_channel_select, *Total_channels.keys())
+        first_channel.pack(side=tk.LEFT)
+
+        second_channel_label = tk.Label(frame_select, text="Select 2nd:", fg='orange')
+        second_channel_label.pack(side=tk.LEFT)
+        second_channel_select = tk.StringVar(value="")
+        second_channel= tk.OptionMenu(frame_select, second_channel_select, *Total_channels.keys())
+        second_channel.pack(side=tk.LEFT)
+
+        third_channel_label = tk.Label(frame_select, text="Select 1st:", fg='green')
+        third_channel_label.pack(side=tk.LEFT)
+        third_channel_select = tk.StringVar(value="")
+        third_channel= tk.OptionMenu(frame_select, third_channel_select, *Total_channels.keys())
+        third_channel.pack(side=tk.LEFT)
+
+        fourth_channel_label = tk.Label(frame_select, text="Select 1st:", fg='red')
+        fourth_channel_label.pack(side=tk.LEFT)
+        fourth_channel_select = tk.StringVar(value="")
+        fourth_channel= tk.OptionMenu(frame_select, fourth_channel_select, *Total_channels.keys())
+        fourth_channel.pack(side=tk.LEFT)
+
+
+        Plot_button = tk.Button()
+
+        frame_select.pack(side=tk.BOTTOM)
         button1 = tk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
